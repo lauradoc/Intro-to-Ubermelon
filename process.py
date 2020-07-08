db203ed3b0f321d27f19e42712bc223f80e310c7
Laura Docherty <laurahdocherty@gmail.com>
2020-07-08 10:46:38 -0500

    changed sales report to Mon

--- process.py
+++ process.py
@@ -2,10 +2,14 @@ log_file = open("um-server-01.txt")
 
 
 def sales_reports(log_file):
+    #creates function to run report
     for line in log_file:
+        #going through each line from the file
         line = line.rstrip()
+        #removes spaces from each line from file
         day = line[0:3]
-        if day == "Tue":
+
+        if day == "Mon":
             print(line)
 
 
